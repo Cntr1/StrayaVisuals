0682b4ceb0e3e881b1252130bc0536e3df610477
Damian <damiansdemel@gmail.com>
2025-03-06 15:41:48 +0530

    Initial Commit
    React app structure

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'straya-visuals',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'straya-visuals',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
